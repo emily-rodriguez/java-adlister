8db75ec41dbcb08bbd7bce2aafacee65773df4e6
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-31 11:54:52 -0500

    updated helloWorldServlet after in class solution

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,11 +10,23 @@ public class HelloWorldServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
-        if (name != null) {
-            out.println("<h1> Hello, " + name + "!</h1>");
-        } else {
-            out.println("<h1>Hello, World!</h1>");
+//        if (name != null) {
+//            out.println("<h1> Hello, " + name + "!</h1>");
+//        } else {
+//            out.println("<h1>Hello, World!</h1>");
+//        }
+
+        //in class solution
+
+        if (name == null) {
+            name = "World";
         }
+
+        out.println("<h1>Hello, " + name + "!</h1>");
+        out.println("<p>Enter your name: </p>");
+        out.println("<form action=\"/hello\">");
+        out.println("<input name=\"name\"/>");
+        out.println("</form>");
     }
 }
 
