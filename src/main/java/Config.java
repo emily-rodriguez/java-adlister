37cc10804839e90b799fde6ae73513ee989b20de
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-05 15:11:44 -0500

    update gitignore

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,3 +1,17 @@
 public class Config {
+    private String url;
+    private String user;
+    private String password;
 
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
