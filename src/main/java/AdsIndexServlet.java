82be35d6aeb7ce170cc8a7a279ab23ff2fbcfae5
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-06 09:51:09 -0500

    handling exceptions where necessary

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -4,17 +4,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Ad> ads = DaoFactory.getAdsDao().all();
-        request.setAttribute("ads", ads);
-        if (request.getSession().getAttribute("user") != null) {
-            request.getRequestDispatcher("./WEB-INF/ads/index.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/login");
+        try {
+            request.setAttribute("ads", DaoFactory.getAdsDao().all());
+            if (request.getSession().getAttribute("user") != null) {
+                request.getRequestDispatcher("./WEB-INF/ads/index.jsp").forward(request, response);
+            } else {
+                response.sendRedirect("/login");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,22 +1,51 @@
 import java.sql.*;
 import com.mysql.jdbc.Driver;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class MySQLAdsDao implements Ads{
+public class MySQLAdsDao implements Ads {
     private Connection connection;
 
-    public MySQLAdsDao(Config config) throws SQLException {
-        DriverManager.registerDriver(new Driver());
-        connection = DriverManager.getConnection(
+    public MySQLAdsDao(Config config){
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
                 config.getUrl(),
                 config.getUser(),
                 config.getPassword()
-        );
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
     @Override
-    public List<Ad> all() {
-       return null;
+    public List<Ad> all(){
+        List<Ad> ads = new ArrayList<>();
+        long id;
+        long user_id;
+        String title;
+        String description;
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            while(rs.next()) {
+                id = rs.getLong("id");
+                user_id = rs.getLong("user_id");
+                title = rs.getString("title");
+                description = rs.getString("description");
+
+                Ad ad = new Ad(id, user_id, title, description);
+                ads.add(ad);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ads;
     }
 
     @Override
